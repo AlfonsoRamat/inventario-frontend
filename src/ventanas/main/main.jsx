087c503e9395a270a369e529b0b6de3b43028598
Roberto Prada <pradarg@gmail.com>
2021-02-08 21:39:16 -0300

    intentando card

--- src/ventanas/main/main.jsx
+++ src/ventanas/main/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from '../../componentes/Navbar/navbar';
 import Inventario from '../inventario/Inventario';
+import { card } from 'react-bootstrap';
+
 import './main.css';
 
 function Main(props) {
@@ -10,7 +12,21 @@ function Main(props) {
         <Navbar />
       </div>
       <div className="main">
-        <Inventario />
+       <div> 
+       <Card style={{ width: '18rem' }}>
+        <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+       <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+         </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+        </Card>
+       </div>
+        <div><Inventario /></div>
       </div>
       <div className="footer">
         <h3><strong>RT Development ©</strong></h3>
