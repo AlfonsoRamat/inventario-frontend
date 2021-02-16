d25ade70d99e05d9fc290f3c4f77973d474db0b9
Alfonso Ramat <alfonso.ramat@gmail.com>
2021-02-15 22:47:09 -0300

    refactorizada pantalla inventario

---
+++ src/extras/configs/TablaInventario.js
@@ -0,0 +1,67 @@
+const customStyles = {
+    rows: {
+      style: {
+        minHeight: '72px', // override the row height
+      }
+    },
+    headCells: {
+      style: {
+        paddingLeft: '8px', // override the cell padding for head cells
+        paddingRight: '8px',
+      },
+    },
+    cells: {
+      style: {
+        paddingLeft: '8px', // override the cell padding for data cells
+        paddingRight: '8px',
+        fontSize: '20px' ,
+      },
+    },
+  };
+
+const columnas = [
+    {
+        name: 'Codigo Interno',
+        selector: 'codigoInterno',
+        sortable: true
+    },
+    {
+        name: 'Codigo de barra',
+        selector: 'codigoPaquete',
+        sortable: true
+    },
+    {
+        name: 'Nombre',
+        selector: 'nombre',
+        sortable: true
+    },
+    {
+        name: 'Descripcion',
+        selector: 'descripcion',
+        sortable: true
+    },
+
+    {
+        name: 'Alerta Min',
+        selector: 'alertaMin',
+        sortable: true
+    },
+    {
+        name: 'Precio de lista',
+        selector: 'precio',
+        sortable: true
+    },
+    {
+        name: 'Estado',
+        selector: 'estado',
+        sortable: true
+    }
+]
+const opcionesdepagina = {
+    rowsPerPageText: 'Filas por pagina',
+    rangeSeparatorText: 'de',
+    selectAllRowsItem: true,
+    selectAllRowsItemText: 'Todo'
+}
+
+export { customStyles, columnas, opcionesdepagina};
--- src/ventanas/inventario/Inventario.jsx
+++ src/ventanas/inventario/Inventario.jsx
@@ -2,80 +2,14 @@ import React, { useEffect, useState } from 'react';
 import './Inventario.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DataTable from 'react-data-table-component';
-import AgregarProductosModal from '../../componentes/agregarProducto/agregarProductoModal';
-
-
-const customStyles = {
-    rows: {
-      style: {
-        minHeight: '72px', // override the row height
-      }
-    },
-    headCells: {
-      style: {
-        paddingLeft: '8px', // override the cell padding for head cells
-        paddingRight: '8px',
-      },
-    },
-    cells: {
-      style: {
-        paddingLeft: '8px', // override the cell padding for data cells
-        paddingRight: '8px',
-        fontSize: '20px' ,
-      },
-    },
-  };
-
-const columnas = [
-    {
-        name: 'Codigo Interno',
-        selector: 'codigoInterno',
-        sortable: true
-    },
-    {
-        name: 'Codigo de barra',
-        selector: 'codigoPaquete',
-        sortable: true
-    },
-    {
-        name: 'Nombre',
-        selector: 'nombre',
-        sortable: true
-    },
-    {
-        name: 'Descripcion',
-        selector: 'descripcion',
-        sortable: true
-    },
-
-    {
-        name: 'Alerta Min',
-        selector: 'alertaMin',
-        sortable: true
-    },
-    {
-        name: 'Precio de lista',
-        selector: 'precio',
-        sortable: true
-    },
-    {
-        name: 'Estado',
-        selector: 'estado',
-        sortable: true
-    }
-]
-const opcionesdepagina = {
-    rowsPerPageText: 'Filas por pagina',
-    rangeSeparatorText: 'de',
-    selectAllRowsItem: true,
-    selectAllRowsItemText: 'Todo'
-}
-
-
-
-
-
+import { columnas, customStyles, opcionesdepagina } from "../../extras/configs/TablaInventario";
+import InventarioCards from "../../componentes/inventarioCards/InventarioCards";
 function Inventario(props) {
+
+    const [modal, setModal] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [items, setItems] = useState([]);
+    const [search, setsearch] = useState("");
 
     function toggleModal() {
         setModal((prev) => prev ? false : true);
@@ -86,12 +20,6 @@ function Inventario(props) {
         toggleModal();
     }
 
-
-    const [modal, setModal] = useState(false);
-    const [selectedItem, setSelectedItem] = useState(null);
-    const [items, setItems] = useState([]);
-    const [search, setsearch] = useState("");
-    
     async function getItems() {
 
         const url = 'http://localhost:3004';
@@ -116,36 +44,7 @@ function Inventario(props) {
         <div className="body">
             <div>
                 <div>
-                    <div className="card-deck">
-                        <div className="card" >
-                            <div className="card-body">
-                                <h5 className="card-title">Productos</h5>
-                                <p className="card-text text-white"  >.</p>
-                                <p className="card-text text-white"  >.</p>
-                                <p className="card-text text-white"  >.</p>
-                                <button type="submit" onClick={toggleModal}>Agregar producto</button>
-                                <AgregarProductosModal modalState={modal} item={selectedItem} toggle={toggleModal} />
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Ordenes de pedido</h5>
-                                <p className="card-text text-white"  >.</p>
-                                <p className="card-text text-white"  >.</p>
-                                <p className="card-text text-white"  >.</p>
-                                <button type="submit" onClick={console.log(`pedido`)}>Hacer pedido</button>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Reservas</h5>
-                                <p className="card-text text-white"  >.</p>
-                                <p className="card-text text-white"  >.</p>
-                                <p className="card-text text-white"  >.</p>
-                                <button type="submit" onClick={console.log(`reservas`)}>Ver</button>
-                            </div>
-                        </div>
-                    </div>
+                <InventarioCards modal={modal} selectedItem={selectedItem} toggleModal={toggleModal} />
                 </div>
                 <div className='titulo-tabla'>
                     <div className='titulo-izq'><h1>Inventario</h1></div>
