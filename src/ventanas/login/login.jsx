b268d9a1c2416e78688f6334e5a105d320b34105
Roberto Prada <pradarg@gmail.com>
2021-03-15 13:34:45 -0300

    correcciomn formato botones y clasname

--- src/ventanas/login/login.jsx
+++ src/ventanas/login/login.jsx
@@ -39,7 +39,7 @@ function LoginScreen({ history }) {
                             {/* <label htmlFor="password">Password</label> */}
                             <Field placeholder="Contraseña" className="login-input" type="password" name="password" />
                             <ErrorMessage className="error" name="password">{msg => <div className="error">{msg}</div>}</ErrorMessage>
-                            <input type="submit" value="Ingresar" />
+                            <input type="submit" value="Ingresar" className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" />
                             {errorMessage ? <div className="error">{errorMessage}</div> : null}
                         </Form>
                     </div>
