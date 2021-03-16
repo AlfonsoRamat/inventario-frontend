483b7f0a161be97e089bd14f251f6a54cf36fecc
Roberto Prada <pradarg@gmail.com>
2021-03-16 12:30:09 -0300

    formato modal producto

--- src/componentes/agregarProducto/agregarProductoModal.jsx
+++ src/componentes/agregarProducto/agregarProductoModal.jsx
@@ -20,7 +20,7 @@ function AgregarProductosModal({ modalState, selectedItem, items, proveedores, t
         precio: 1,
         precioVenta: 1,
         cantidad: 1,
-        proveedorId:'',
+        proveedorId: '',
     };
 
     const submitForm = async (values, actions) => {
@@ -38,8 +38,8 @@ function AgregarProductosModal({ modalState, selectedItem, items, proveedores, t
         <Modal isOpen={modalState} onRequestClose={toggle} style={
             {
                 content: {
-                    width: '70%',
-                    height: '90%',
+                    width: '75%',
+                    height: '80%',
                     top: '50%',
                     left: '50%',
                     right: 'auto',
@@ -53,92 +53,134 @@ function AgregarProductosModal({ modalState, selectedItem, items, proveedores, t
                 <Form className="formulario-modal">
                     <div className="rounded-t bg-white mb-0 px-6 py-6">
                         <div className="text-center flex justify-between">
-                            <h6 className="text-gray-800 text-xl font-bold">Agregar Productos</h6>
+                            <h4 className="text-gray-800 text-xl font-bold">Agregar Productos</h4>
                             <div className="inputs">
 
                                 <div className="left-inputs">
-                                    <label htmlFor="codInterno">Codigo interno</label>
-                                    <Field type="text" id="codInterno" name="codInterno" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
-                                    <ErrorMessage name="codInterno">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+                                    <div className="formatimput">
+                                        <label htmlFor="codInterno" >Codigo interno</label>
+                                        <Field type="text" id="codInterno" name="codInterno" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
+                                        <ErrorMessage name="codInterno">{msg => <div className="error">{msg}</div>}</ErrorMessage>
 
-                                    <label htmlFor="codigoPaquete">Codigo de paquete</label>
-                                    <Field type="text" id="codigoPaquete" name="codigoPaquete" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
-                                    <ErrorMessage name="codigoPaquete">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+                                    </div>
+                                    <div className="formatimput">
+                                        <label htmlFor="codigoPaquete">Codigo de paquete</label>
+                                        <Field type="text" id="codigoPaquete" name="codigoPaquete" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
+                                        <ErrorMessage name="codigoPaquete">{msg => <div className="error">{msg}</div>}</ErrorMessage>
 
-                                    <label htmlFor="nombre">Nombre</label>
-                                    <Field type="text" id="nombre" name="nombre" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
-                                    <ErrorMessage name="nombre">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+                                    </div>
+                                    <div className="formatimput">
+                                        <label htmlFor="nombre">Nombre</label>
+                                        <Field type="text" id="nombre" name="nombre" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
+                                        <ErrorMessage name="nombre">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+                                    </div>
+
+                                    <div className="formatimput">
+
+                                        <label htmlFor="descripcion">Descripcion</label>
+                                        <Field as="textarea" id="descripcion" name="descripcion" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
+                                        <ErrorMessage name="descripcion">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+
+                                    </div>
 
 
                                 </div>
                                 <div className="right-inputs">
+                                    <div className="formatimput">
+                                        <label htmlFor="precio">Costo</label>
+                                        <Field type="text" id="precio" name="precio" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
+                                        <ErrorMessage name="precio">{msg => <div className="error">{msg}</div>}</ErrorMessage>
 
 
+                                    </div>
+                                    <div className="formatimput">
 
-                                    <label htmlFor="precio">Costo</label>
-                                    <Field type="text" id="precio" name="precio" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
-                                    <ErrorMessage name="precio">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+                                        <label htmlFor="precioVenta">Precio de venta</label>
+                                        <Field type="text" id="precioVenta" name="precioVenta" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
+                                        <ErrorMessage name="precioVenta">{msg => <div className="error">{msg}</div>}</ErrorMessage>
 
-                                    <label htmlFor="precioVenta">Precio de venta</label>
-                                    <Field type="text" id="precioVenta" name="precioVenta" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
-                                    <ErrorMessage name="precioVenta">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+                                    </div>
+                                    <div className="formatimput">
+                                        <label htmlFor="cantidad">Cantidad</label>
+                                        <Field type="text" id="cantidad" name="cantidad" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
+                                        <ErrorMessage name="cantidad">{msg => <div className="error">{msg}</div>}</ErrorMessage>
 
-                                    <label htmlFor="descripcion">Descripcion</label>
-                                    <Field as="textarea" id="descripcion" name="descripcion" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
-                                    <ErrorMessage name="descripcion">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+
+                                    </div>
+
+
+                                    <div className="formatimput">
+                                        <label htmlFor="alertaMin">Alerta minima</label>
+                                        <Field type="text" id="alertaMin" name="alertaMin" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
+                                        <ErrorMessage name="alertaMin">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+
+
+                                    </div>
+
 
 
                                 </div>
                                 <div className="tercercol">
 
-                                    <label htmlFor="cantidad">Cantidad</label>
-                                    <Field type="text" id="cantidad" name="cantidad" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
-                                    <ErrorMessage name="cantidad">{msg => <div className="error">{msg}</div>}</ErrorMessage>
 
-                                    <label htmlFor="alertaMin">Alerta minima</label>
-                                    <Field type="text" id="alertaMin" name="alertaMin" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
-                                    <ErrorMessage name="alertaMin">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+                                    <div className="formatimput">
+                                        <label htmlFor="proveedorId">Proveedor</label>
+                                        <Field as="select" id="proveedorId" name="proveedorId" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" >
+                                            <option value=''>Seleccione una opcion</option>
+                                            {
+                                                proveedores.map(proveedor => <option key={proveedor.id} value={proveedor.id}>{proveedor.nombre}</option>)
+                                            }
+                                        </Field>
+                                        <ErrorMessage name="proveedorId">{msg => <div className="error">{msg}</div>}</ErrorMessage>
 
 
-                                    <label htmlFor="proveedorId">Proveedor</label>
-                                    <Field as="select" id="proveedorId" name="proveedorId" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" >
-                                        <option value=''>Seleccione una opcion</option>
-                                        {
-                                            proveedores.map(proveedor => <option key={proveedor.id} value={proveedor.id}>{proveedor.nombre}</option>)
-                                        }
-                                    </Field>
-                                    <ErrorMessage name="proveedorId">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+                                    </div>
 
+                                    <div className="formatimput">
+                                        <label htmlFor="estado">Estado</label>
+                                        <Field as="select" id="estado" name="estado" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150">
+                                            <option value=''>Seleccione una opcion</option>
+                                            <option value="BUENO">BUENO</option>
+                                            <option value="DEFECTUOSO">DEFECTUOSO</option>
+                                            <option value="RESERVADO">RESERVADO</option>
+                                        </Field>
+                                        <ErrorMessage name="estado">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+
+
+                                    </div>
+
+                                    <div className="formatimput">
+
+                                        <label htmlFor="ubicacion">Ubicacion</label>
+                                        <Field as="select" id="ubicacion" name="ubicacion" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150">
+                                            <option value=''>Seleccione una opcion</option>
+                                            <option value="PROVEEDOR">PROVEEDOR</option>
+                                            <option value="DEPOSITO">DEPOSITO</option>
+                                            <option value="LOCAL">LOCAL</option>
+                                        </Field>
+                                        <ErrorMessage name="ubicacion">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+
+                                    </div>
 
                                 </div>
                                 <div className="cuartaCol">
-                                    <label htmlFor="estado">Estado</label>
-                                    <Field as="select" id="estado" name="estado" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150">
-                                        <option value=''>Seleccione una opcion</option>
-                                        <option value="BUENO">BUENO</option>
-                                        <option value="DEFECTUOSO">DEFECTUOSO</option>
-                                        <option value="RESERVADO">RESERVADO</option>
-                                    </Field>
-                                    <ErrorMessage name="estado">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+
+                                    <div className="formatimput">
+                                        <label htmlFor="alertaMax">Alerta maxima</label>
+
+                                        <Field ttype="text" id="alertaMax" name="alertaMax" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
+                                        <ErrorMessage name="alertaMax">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+
+                                    </div>
+                                    <div className="formatimput">
+
+                                        <label htmlFor="marca">Marca</label>
+                                        <Field type="text" id="marca" name="marca" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
+                                        <ErrorMessage name="marca">{msg => <div className="error">{msg}</div>}</ErrorMessage>
+
+                                    </div>
 
 
-
-                                    <label htmlFor="alertaMax">Alerta maxima</label>
-                                    <Field ttype="text" id="alertaMax" name="alertaMax" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
-                                    <ErrorMessage name="alertaMax">{msg => <div className="error">{msg}</div>}</ErrorMessage>
-
-                                    <label htmlFor="ubicacion">Ubicacion</label>
-                                    <Field as="select" id="ubicacion" name="ubicacion" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150">
-                                        <option value=''>Seleccione una opcion</option>
-                                        <option value="PROVEEDOR">PROVEEDOR</option>
-                                        <option value="DEPOSITO">DEPOSITO</option>
-                                        <option value="LOCAL">LOCAL</option>
-                                    </Field>
-                                    <ErrorMessage name="ubicacion">{msg => <div className="error">{msg}</div>}</ErrorMessage>
-
-                                    <label htmlFor="marca">Marca</label>
-                                    <Field type="text" id="marca" name="marca" className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
-                                    <ErrorMessage name="marca">{msg => <div className="error">{msg}</div>}</ErrorMessage>
 
                                 </div>
                             </div>
