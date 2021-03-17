a366bacef73d6ced6dc1c09d428eea2238a04fda
Roberto Prada <pradarg@gmail.com>
2021-03-17 15:28:38 -0300

    correcion stilos agregar producto

--- src/componentes/agregarprovedor/agregarProvedorModal.jsx
+++ src/componentes/agregarprovedor/agregarProvedorModal.jsx
@@ -41,38 +41,42 @@ function AgregarProvedorModal({ toogle, setoggle, proveedores, fadditem }) {
                 }} >
                 <Formik initialValues={initialValues} validationSchema={null} onSubmit={handleAgregar}>
                     <Form className="formulario-povedor">
+                    <div className="rounded-t bg-white mb-0 px-6 py-6">
+                        <div className="text-center flex justify-between">
+                            <h4 className="text-gray-800 text-xl font-bold">Agregar Proveedor</h4>
+
                         <div className="inputsprovedor">
                             <div className="imputDatos">
                                 <div className="leftinputs">
                                     <label htmlFor="codigoInterno">Codigo interno</label>
-                                    <Field type="text" id="codigoInterno" name="codigoInterno" />
+                                    <Field type="text" id="codigoInterno" name="codigoInterno" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"/>
                                     <ErrorMessage name="codigoInterno">{msg => <div className="error">{msg}</div>}</ErrorMessage>
 
                                     <label htmlFor="nombre">Nombre</label>
-                                    <Field type="text" id="nombre" name="nombre" />
+                                    <Field type="text" id="nombre" name="nombre" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"/>
                                     <ErrorMessage name="nombre">{msg => <div className="error">{msg}</div>}</ErrorMessage>
 
                                     <label htmlFor="telefono">Telefono</label>
-                                    <Field type="text" id="telefono" name="telefono" />
+                                    <Field type="text" id="telefono" name="telefono" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"/>
                                     <ErrorMessage name="telefono">{msg => <div className="error">{msg}</div>}</ErrorMessage>
                                 </div>
                                 <div className="rightinputs">
                                 <label htmlFor="telefono">Descripcion</label>
-                                    <Field as="textarea" id="descripcion" name="descripcion" />
+                                    <Field as="textarea" id="descripcion" name="descripcion" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"/>
                                     <ErrorMessage name="descripcion">{msg => <div className="error">{msg}</div>}</ErrorMessage>
                                     
                                     <label htmlFor="email">Email</label>
-                                    <Field type="text" id="email" name="email" />
+                                    <Field type="text" id="email" name="email" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"/>
                                     <ErrorMessage name="email">{msg => <div className="error">{msg}</div>}</ErrorMessage>
                                 </div>
                             </div>
 
                             <div className="modal-botones">
-                                <button className="botones" type="submit">Agregar</button>
-                                <button className="botones" type="reset" value="finalizar" onClick={setoggle}>Finalizar</button>
+                                <button  className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="submit">Agregar</button>
+                                <button  className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" type="reset" value="finalizar" onClick={setoggle}>Finalizar</button>
                             </div>
                         </div>
-
+</div> </div>
 
                     </Form>
                 </Formik>
