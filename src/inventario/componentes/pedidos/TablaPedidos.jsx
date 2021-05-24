50e3a48124a03e4dafc628dcd0833de1f80ac26a
PradaRG <pradarg@gmail.com>
2021-05-24 02:37:17 -0300

    correcion style y filtro

--- src/inventario/componentes/pedidos/TablaPedidos.jsx
+++ src/inventario/componentes/pedidos/TablaPedidos.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { InventarioContext } from '../../inventario/InventarioContext';
 import DataTable from 'react-data-table-component';
-import { PedidoColumns, AlertaColumns,customStyles,  opcionesdepagina } from './Pedido.configs';
+import { PedidoColumns, AlertaColumns,customStyles,  opcionesdepagina,conditionalRowStyles } from './Pedido.configs';
 import { Form, Formik, Field, ErrorMessage } from "formik";
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
@@ -14,14 +14,19 @@ function TablaPedidos() {
 
     const [filtro, setFiltro] = useState(productos);
     const [filtrarVacios, setFiltrarVacios] = useState(false);
-
-    const productosConAlerta = filtro.filter(prod => {
-        let value = prod.Stocks.reduce((total, actual) => {
-            return total + parseInt(actual.cantidad);
-        }, 0);
-        if ((value === 0 && !filtrarVacios) || value <= prod.alertaMin) return true;
-        else return false;
-    });
+    function filtrarstock(){
+        setFiltrarVacios(!filtrarVacios);
+        const listas = filtrar(productos).filter(prod => {
+            let value = prod.Stocks.reduce((total, actual) => {
+                return total + parseInt(actual.cantidad);
+            }, 0);
+            
+            if ( value <= prod.alertaMin){if(value === 0 && !filtrarVacios) return true; else return false; } 
+            else return false;
+        });
+    setFiltro( listas);
+        console.log("agregar ",filtro)
+    }
 
      function  filtrar(rows) {
 
@@ -53,15 +58,16 @@ function TablaPedidos() {
 
 
                 <label htmlFor="productosEnCero">Incluir productos sin stock?</label>
-                <input type="checkbox" checked={filtrarVacios} name="productosEnCero" onChange={() => { setFiltrarVacios(!filtrarVacios) }} id="productosEnCero" />
+                <input type="checkbox" checked={filtrarVacios} name="productosEnCero" onChange={() => { filtrarstock() }} id="productosEnCero" />
             </div>
             <div className="split">
 
                 <div className="columna">
                     <h2 className="subtitle">Productos en Alerta</h2>
                     <DataTable
+                    conditionalRowStyles={conditionalRowStyles}
                         columns={AlertaColumns}
-                        data={filtrar(productosConAlerta)}
+                        data={filtro}
                         pagination
                         paginationComponentOptions={opcionesdepagina}
                         customStyles={customStyles}
