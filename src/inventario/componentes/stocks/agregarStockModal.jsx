f26c326d923a340e28a02360486e67840449681c
pradarg <pradarg@gmail.com>
2021-05-24 21:47:58 -0300

    style agregar stock

--- src/inventario/componentes/stocks/agregarStockModal.jsx
+++ src/inventario/componentes/stocks/agregarStockModal.jsx
@@ -32,8 +32,8 @@ function AgregarStockModal({ modal, toggleModal, userSelection, setUserSelection
         <Modal isOpen={modal} onRequestClose={toggleModal} style={
             {
                 content: {
-                    width: '50%',
-                    height: '40%',
+                    width: '30%',
+                    height: '45%',
                     top: '50%',
                     left: '50%',
                     right: 'auto',
@@ -42,11 +42,11 @@ function AgregarStockModal({ modal, toggleModal, userSelection, setUserSelection
                     transform: 'translate(-50%, -50%)'
                 }
             }} >
-                <div className="formularioModal">
+                
                 <Formik initialValues={initialValues} validationSchema={StockFormValidation} onSubmit={submitForm}>
                 
                 <Form>
-                    
+                <div className="formularioStock">
                         <label htmlFor="cantidad">Cantidad</label>
                         <Field type="text" id="cantidad" name="cantidad" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
                         <ErrorMessage name="cantidad">{msg => <div className="error">{msg}</div>}</ErrorMessage>
@@ -56,12 +56,16 @@ function AgregarStockModal({ modal, toggleModal, userSelection, setUserSelection
                         <Field type="text" id="precioCompra" name="precioCompra" className="px-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150" />
                         <ErrorMessage name="precioCompra">{msg => <div className="error">{msg}</div>}</ErrorMessage>
                     
-                    
-                        <button className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                    <div className="formularioBotones">
+                    <button className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
                             type="submit">Agregar</button>
-                    
+                                                    <button className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                            type="reset" onClick={toggleModal}>Cancelar</button>
+                    </div>
+                
+                    </div>
                 </Form>
-            </Formik></div>
+            </Formik>
        
                 
              </Modal>
