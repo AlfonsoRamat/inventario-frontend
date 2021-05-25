65857b7f410543ab0138371934fff36cd10d5e65
pradarg <pradarg@gmail.com>
2021-05-25 07:06:05 -0300

    correcion tabla expandida

--- src/inventario/componentes/items/ExpandableComponent.jsx
+++ src/inventario/componentes/items/ExpandableComponent.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import DataTable from "react-data-table-component";
 import { BsTrash } from "react-icons/bs";
 import AxiosInstance from '../../../shared/configs/AxiosInstance';
 
 function ExpandableComponent({ data, productosDispatch }) {
-
+const [bandera, setBandera] = useState(false)
   const reposiciones = data.Stocks;
+async function borrarrow(row){
 
+
+  await AxiosInstance().delete('/productos/repo/', { data: { id: row.id } })
+  .then(res => {
+    const stocks = reposiciones.filter((value) => {
+      return value.id !== row.id;
+    });
+    data.Stocks = stocks;
+    productosDispatch({ type: 'modificar', payload: data });
+    
+  }).catch(err => console.log(data.stocks));
+  setBandera(!bandera)
+}
+useEffect(() => {
+
+}, [bandera])
   return (
     <DataTable
       data={reposiciones}
@@ -52,14 +68,7 @@ function ExpandableComponent({ data, productosDispatch }) {
 
             cell: row => <BsTrash onClick={() => {
               if (window.confirm(`Seguro que desea eliminar esta reposicion `)) {
-                AxiosInstance().delete('/productos/repo/', { data: { id: row.id } })
-                  .then(res => {
-                    const stocks = reposiciones.filter((value) => {
-                      return value.id !== row.id;
-                    });
-                    data.Stocks = stocks;
-                    productosDispatch({ type: 'modificar', payload: data });
-                  }).catch(err => console.log(err));
+                    borrarrow(row);
               }
             }} />,
           }
