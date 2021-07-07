0029ce41b25c4e321c8fb0423a5c4c0fbaa22a1b
pradarg <pradarg@gmail.com>
2021-07-07 00:02:17 -0300

    correcion card producto descarga excel

--- src/reportes/componentes/reportesCard/CARD_LISTA_PRODUCTO.js
+++ src/reportes/componentes/reportesCard/CARD_LISTA_PRODUCTO.js
@@ -1,59 +1,64 @@
-import React, { useContext,useEffect,useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { columnasVenta, customStyles, opcionesdepagina } from "../../../shared/configs/TablaInventario";
 // components
 import ReactExport from 'react-data-export';
 import DataTable from 'react-data-table-component';
-import{ReporteContext} from "../../ReportesContext";
+import { ReporteContext } from "../../ReportesContext";
 
 export default function CARD_LISTA_PRODUCTOS() {
     const ExcelFile = ReactExport.ExcelFile;
     const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
-    const{ setSearch,buscar,search, Productos,ventaSelected }=  useContext(ReporteContext);
-  
+    const { setSearch, buscar, search, Productos, ventaSelected } = useContext(ReporteContext);
 
-    const [reset,setReset]=useState(false);
+
+    const [reset, setReset] = useState(false);
     const [productos, setProducto] = useState([]);
-    useEffect(() => {
-        setProducto([]);
-        if (ventaSelected) {
-            ventaSelected.ItemsVenta.forEach(item => {
-                const aux = item.Producto;
-                aux.cantidad=item.cantidad;
-                setProducto(prev => [...prev, aux]);
-                console.log("aux",aux);
-                          });
-                         
-                                if(!reset){setReset(!reset)} 
-        }else {  setProducto(Productos);if(!reset){setReset(!reset)} }
-    }, [reset])
+
+
     const DataSet = [
         {
             columns: [
 
-                { title: "Codigo Interno", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } }, // width in pixels
+               
                 { title: "Codigo de barra", style: { font: { sz: "18", bold: true } }, width: { wch: 30 } }, // width in characters
                 { title: "Nombre", style: { font: { sz: "18", bold: true } }, width: { wpx: 100 } }, // width in pixels
                 { title: "Descripcion", style: { font: { sz: "18", bold: true } }, width: { wpx: 300 } }, // width in pixels
-                ventaSelected?null: { title: "Cantidad", style: { font: { sz: "18", bold: true } }, width: { wpx: 100 } }, // width in pixels
+                { title: "Cantidad", style: { font: { sz: "18", bold: true } }, width: { wpx: 100 } }, // width in pixels
                 { title: "Precio", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } }, // width in pixels
 
 
 
             ],
             data: buscar(productos).map((data) => [
-                { value: data.codInterno, style: { font: { sz: "14" } } },
+               
                 { value: data.codigoPaquete, style: { font: { sz: "14" } } },
                 { value: data.nombre, style: { font: { sz: "14" } } },
                 { value: data.descripcion, style: { font: { sz: "14" } } },
-                ventaSelected?null:{ value:  data.Stocks.reduce((total, actual) => {
-                    return total + parseFloat(actual.cantidad);
-                  }, 0), style: { font: { sz: "14" } } },
+               !ventaSelected? {
+                    value: data.Stocks.reduce((total, actual) => {
+                        return total + parseFloat(actual.cantidad);
+                    }, 0), style: { font: { sz: "14" } }
+                }:{
+                    value: data.cantidad, style: { font: { sz: "14" } }
+                },
                 { value: data.precioVenta, style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "eb1207" } } } },
             ])
         }
     ]
 
+    useEffect(() => {
 
+        if (ventaSelected) {
+            setProducto([]);
+            ventaSelected.ItemsVenta.forEach(item => {
+                const aux = item.Producto;
+                aux.cantidad = item.cantidad;
+                setProducto(prev => [...prev, aux]);
+                
+            });
+            if (!reset) { setReset(!reset) }
+        } else { setProducto(Productos); if (!reset) { setReset(!reset);  console.log(Productos)}  }
+    }, [reset,Productos])
     return (
         <>
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
@@ -64,15 +69,15 @@ export default function CARD_LISTA_PRODUCTOS() {
                             {(Productos && Productos.length !== 0) ?
                                 <div className="float-right m-3">
                                     <div className="input-icono">
-                                        <input type="text" value={search} onChange={(e) => setSearch(e.target.value) } placeholder="Buscar..." />
+                                        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Buscar..." />
                                     </div>
                                 </div> : null}
                         </div>
-                        {ventaSelected?null: <ExcelFile
+                       <ExcelFile
                             filename="productos Data"
                             element={<button type="button" className="">Descargar informacion</button>}>
                             <ExcelSheet dataSet={DataSet} name="tabla de productos" />
-                        </ExcelFile>}
+                        </ExcelFile>
 
                         <div className="table-responsive">
                             <DataTable
@@ -87,7 +92,7 @@ export default function CARD_LISTA_PRODUCTOS() {
                                 noDataComponent={<div>No hay informacion disponible para mostrar</div>}
                                 customStyles={customStyles}
                             />
-                            
+
 
                         </div>
                     </div>
